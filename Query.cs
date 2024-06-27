443a7146c671c1f9240f4b83187ce1cc8b926ead
dimitris.zarachanis <dimitris.zarachanis@dialecticanet.com>
2024-06-27 14:25:14 +0300

    init commit

---
+++ Query.cs
@@ -0,0 +1,25 @@
+
+public class Book
+{
+    public string? Title {get; set;}
+
+    public Author? Author {get; set;}
+}
+
+public class Author
+{
+    public string? Name {get; set;}
+}
+
+public class Query
+{
+    public Book GetBook() =>
+        new()
+        {
+            Title = "C# in depth.",
+            Author = new Author
+            {
+                Name = "Jon Skeet"
+            }
+        };
+}
